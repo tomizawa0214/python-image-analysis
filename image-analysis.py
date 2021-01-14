9a996e0a8e1cf2c4f5ddc48474bfd0077bcc858c
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-01-14 19:24:01 +0900

    画像

--- image-analysis.py
+++ image-analysis.py
@@ -9,3 +9,10 @@ img_bw = imread('tapioca_drink.jpg', 2) # ,2は白黒で読み込む合図
 # 一定の明るさ(この場合は120)より暗い部分のみ残してみる
 img_mod = where(img_bw > 120 , 0, img_bw)
 pyplot.imshow(img_mod)
+pyplot.show()
+
+# import cv2
+
+# filename = "tapioca_drink.jpg"
+# imgCV = cv2.imread(filename) 
+# cv2.imshow("image", imgCV)
